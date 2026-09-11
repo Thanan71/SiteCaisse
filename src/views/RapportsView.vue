07de4b311d8bfc0840bfcf853bb63600c3a3e00d
Thanan71 <38879178+Thanan71@users.noreply.github.com>
2026-09-11 16:50:35 +0200

    refactor: afficher invité dans les rapports

--- src/views/RapportsView.vue
+++ src/views/RapportsView.vue
@@ -29,7 +29,7 @@
               {{ formatArtisanSelectLabel(a) }}
             </option>
           </optgroup>
-          <optgroup label="Artisans temporaires">
+          <optgroup label="Invités">
             <option
               v-for="a in temporaires"
               :key="a.id"
@@ -102,7 +102,7 @@
             :total-cb="rapportsStore.totalGlobal.total_cb"
             :total-commission="rapportsStore.totalGlobal.total_commission"
             :commission-detail="rapportsStore.totalGlobal.total_commission > 0 && rapportsStore.totalAllParams
-              ? `Taux généraux : Permanent ${rapportsStore.totalAllParams.commission_cb_permanent}% / Temporaire ${rapportsStore.totalAllParams.commission_cb_temporaire}%`
+              ? `Taux généraux : Permanent ${rapportsStore.totalAllParams.commission_cb_permanent}% / Invité ${rapportsStore.totalAllParams.commission_cb_temporaire}%`
               : ''"
             commission-label-suffix=" totale"
             variant="primary"
@@ -158,7 +158,7 @@
                 :total-cb="rapportsStore.rapportMois.total.total_cb"
                 :total-commission="rapportsStore.rapportMois.total.total_commission"
                 :commission-detail="rapportsStore.rapportMois.total.total_commission > 0 && rapportsStore.rapportMois.parametres
-                  ? `Taux généraux : Permanent ${rapportsStore.rapportMois.parametres.commission_cb_permanent}% / Temporaire ${rapportsStore.rapportMois.parametres.commission_cb_temporaire}%`
+                  ? `Taux généraux : Permanent ${rapportsStore.rapportMois.parametres.commission_cb_permanent}% / Invité ${rapportsStore.rapportMois.parametres.commission_cb_temporaire}%`
                   : ''"
                 variant="warning"
               />
@@ -248,7 +248,7 @@
                 :total-cb="selectedArtisanRapportMoisGroup.summary.total_cb"
                 :total-commission="selectedArtisanRapportMoisGroup.summary.total_commission"
                 :commission-detail="selectedArtisanRapportMoisGroup.summary.total_commission > 0 && rapportsStore.rapportMois?.parametres
-                  ? `Taux généraux : Permanent ${rapportsStore.rapportMois.parametres.commission_cb_permanent}% / Temporaire ${rapportsStore.rapportMois.parametres.commission_cb_temporaire}%`
+                  ? `Taux généraux : Permanent ${rapportsStore.rapportMois.parametres.commission_cb_permanent}% / Invité ${rapportsStore.rapportMois.parametres.commission_cb_temporaire}%`
                   : ''"
                 commission-label-suffix=" totale"
                 variant="warning"
